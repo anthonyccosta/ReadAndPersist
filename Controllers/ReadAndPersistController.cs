0f05e20cf0b7dddc1084a1b0e8ce1c48654ed02f
ccosta.anthony@gmail.com <ccosta.anthony@gmail.com>
2024-06-02 22:20:14 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ReadAndPersistController.cs
@@ -0,0 +1,25 @@
+﻿using Services;
+using Models;
+
+namespace Controllers
+{
+    public class ReadAndPersistController
+    {
+        private ReadAndPersistController _service;
+
+        public ReadAndPersistController()
+        {
+            _service = new();
+        }
+
+        public List<Radar> LoadData()
+        {
+            return _service.LoadData();
+        }
+
+        public bool InsertData(List<Radar> list)
+        {
+            return _service.InsertData(list);
+        }
+    }
+}
---
+++ ReadAndPersist/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Conectando ao MongoDB...");
+
+// Movendo os dados
+mongoConnectionString repositorio = new mongoConnectionString();
+repositorio.TransferirDadosParaMongo();
+
+Console.WriteLine("Dados salvos com sucesso!");
